dd7c5b6ad9cc92bc859735d233337a4672ef6981
Christian <11605892+chrisulloa@users.noreply.github.com>
2019-01-04 15:40:22 -0800

    Update core.clj

--- src/rarbol/core.clj
+++ src/rarbol/core.clj
@@ -1,6 +1,8 @@
 (ns rarbol.core
   (:require [rarbol.shape :refer :all]
-            [rarbol.zipper :refer :all])
+            [rarbol.zipper :refer :all]
+            [rarbol.visitor :refer :all]
+            [rarbol.util :refer :all])
   (:gen-class))
 
 (defn -main [])
