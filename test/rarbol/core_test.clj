89e8d1337200ed4585f100ff9f27023c4bb37e05
Christian Gonzalez <chg003@ucsd.edu>
2019-01-05 13:17:19 -0800

    Wrote more tests for shape functions.

--- test/rarbol/core_test.clj
+++ test/rarbol/core_test.clj
@@ -5,11 +5,11 @@
             [rarbol.util :refer :all]
             [rarbol.visitor :refer :all]))
 
-(deftest fast-contains
+(deftest test-fast-contains?
   (testing "Fast contains function."
     (is (= true (fast-contains? [10 15 30 5] 30)))))
 
-(deftest visitors
+(deftest test-visitors
   (testing "Visitor functions."
     (let [child1 (map->Rectangle
                    {:x0 0 :x1 50
@@ -37,3 +37,37 @@
       (is (= nil (node-contains-shape-finder tree (->Point 300 300))))
       (is (= #{(->Point 1 1) (->Point 5 5) (->Rectangle 10 15 10 15)}
              (set (enveloped-shapes-collector tree (->Rectangle 0 20 0 20))))))))
+
+(deftest test-minimum-bounding-rectangle
+  (testing "Minimum bounding rectangle."
+    (is (= (->Rectangle -10 55 -15 60)
+           (minimum-bounding-rectangle [(->Point 0 0)
+                                        (->Point 55 60)
+                                        (->Point -10 -15)])))
+    (is (= (->Rectangle -100 100 -300 300)
+           (minimum-bounding-rectangle [(->Point -100 -300)
+                                        (->Point 100 300)
+                                        (->Rectangle 55 60 25 100)])))
+    (is (= (->Rectangle 100 100 150 150)
+           (minimum-bounding-rectangle [(->Rectangle 100 100 150 150)])))))
+
+(deftest test-intersects?
+  (testing "Intersects function."
+    (is (= true (intersects? (->Rectangle 100 150 25 300)
+                             (->Rectangle 50 300 40 500))))
+    (is (= false (intersects? (->Rectangle 50 60 50 60)
+                              (->Rectangle 10 15 10 15))))
+    (is (= true (intersects? (->Rectangle 100 300 100 300)
+                             (->Rectangle 150 250 150 250))))))
+
+(deftest test-envelops?
+  (testing "Envelops function."
+    (is (= true (envelops? (->Rectangle 100 300 100 300)
+                           (->Rectangle 150 250 150 250))))
+    (is (= false (envelops? (->Rectangle 100 150 25 300)
+                            (->Rectangle 50 300 40 500))))))
+
+(deftest test-collect-points
+  (testing "Collect points function."
+    (is (= {:xs [0] :ys [3]} (collect-points (->Point 0 3))))
+    (is (= {:xs [0 10] :ys [5 15]} (collect-points (->Rectangle 0 10 5 15))))))
