c827bffe0eea7b53c4ed38ccceb67349a258a127
Christian Gonzalez <chg003@ucsd.edu>
2019-01-26 22:07:48 -0800

    Removed json from dependencies

--- project.clj
+++ project.clj
@@ -7,5 +7,4 @@
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.github.davidmoten/rtree "0.8.6"]
-                 [org.clojure/data.json "0.2.6"]])
+                 [com.github.davidmoten/rtree "0.8.6"]])
